5867a37341bf09f9f19c92e161ddd7bd6d9c641c
dhamanp10 <prateekdhaman10@gmail.com>
2022-07-10 09:14:15 +0530

    up

--- splitbills/settings.py
+++ splitbills/settings.py
@@ -131,3 +131,4 @@ MEDIA_URL = "/images/download/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
